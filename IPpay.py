51ff08bd8f93b81e5e88a7452b75f29850ab3b99
u1and0 <e01.ando60@gmail.com>
2016-05-14 23:48:42 +0900

    first commit

---
+++ IPpay.py
@@ -0,0 +1,49 @@
+'''
+IP電話アプリの料金をグラフ化
+'''
+
+
+
+
+
+timeSec=[i for i in range(360)]
+plan={'050plus':[300,9,29,60],
+		'fusionIP':[0,8,8,30],
+		'LaLa Call':[100,6,28,60],
+		'G-Call050':[280,5,10,60],
+		'050Call':[0,2,2,60],
+		'050free':[0,3.99,4.33,60],
+		'ServersMan 050':[300,7.5,40,180],
+		'BIGLOBEフォン・モバイル':[300,8,30,60],
+		'FleaLine Light':[400,8,30,60],
+		'BlueSIPフォン':[600,21,21,60]
+		}
+
+
+
+
+# pay=[ for i in ]
+# print(timeSec)
+k=0
+for sev in plan.keys():
+	j,cell,fix=0,[],[]
+	for i in timeSec:
+		cell.append(plan[sev][0]+plan[sev][1]*j)
+		fix.append(plan[sev][0]+plan[sev][2]*j)
+		if i%plan[sev][3]==0:j+=1
+	plan[sev].append(cell)
+	# plan[sev].append(fix)
+	print(plan[sev][4])
+	# print(plan[sev][5])
+	k+=1
+	import matplotlib.pyplot as plt
+	dashtype='-' if k<7 else '--'
+	plt.plot(list(map(lambda re: re/60,timeSec)),plan[sev][4],dashtype,label=sev+' cell')
+	# plt.plot(list(map(lambda re: re/60,timeSec)),plan[sev][5],label=sev+' fix')
+plt.legend(bbox_to_anchor=(1, 1), loc='best', borderaxespad=0,fontsize='small')
+plt.subplots_adjust(right=0.75)
+plt.xlabel('minuits')
+plt.ylabel('Yen')
+plt.show()
+
+
