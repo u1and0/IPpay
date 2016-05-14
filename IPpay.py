eacd92a8c5c9b68e919676bd516a982984b0f9dd
u1and0 <e01.ando60@gmail.com>
2016-05-14 23:55:27 +0900

    README wrote

--- IPpay.py
+++ IPpay.py
@@ -1,6 +1,23 @@
 '''
+## IPpay ver1.0
+
+__UPDATE1.0__
+First commit
+
+__USAGE__
+Just build
+
+__INTRODUCTION__
 IP電話アプリの料金をグラフ化
-q
+
+__ACTION__
+http://s-dentoku.mobile-runner.com/hikaku
+の料金をグラフ化してくれる
+planに料金表
+
+
+__TODO__
+携帯と固定電話を別々にグラフ化
 '''
 
 
